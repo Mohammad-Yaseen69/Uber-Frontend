2f8d2e412df35deeab1276db84634b7864cd5732
Mohammad Yaseen <yaseenyaseenali69@gmail.com>
2025-01-29 01:54:33 +0500

    Add - confirm password check

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -10,9 +10,6 @@ const Register = () => {
   const [showPassword, setShowPassword] = useState(false)
   const [showConfirmPass, setShowConfirmPass] = useState(false)
   const [otpSent, setOtpSent] = useState(false)
-  const [correctOtp, setCorrectOtp] = useState(false)
-  const [otpVerified, setOtpVerified] = useState(false)
-  const [error, setError] = useState(null)
   const [loading, setLoading] = useState(false)
   const [data, setData] = useState({
     email: "",
@@ -28,41 +25,53 @@ const Register = () => {
       setStep(prev => prev += 1)
       return
     } else {
-      toast.dismiss()
-      const loadingToastId = toast.loading("Loading...", { containerId: "app-toast-container" });
-      const response = await fetchData("users/register", {
-        method: "POST",
-        data: data
-      })
-
-      console.log(response)
-
-      if (response.isResponseOk) {
-        toast.update(loadingToastId, {
-          render: "Success! Please check you email for otp",
-          type: 'success',
-          isLoading: false,
-          autoClose: 1900,
+      if (data.password !== data.confirmPassword) {
+        toast.error("Comfirm Password not matching", {
           containerId: "app-toast-container",
-          style: { backgroundColor: "lightgreen" }
-        });
-        setTimeout(() => {
-          setOtpSent(true)
-        }, 2000)
-      }
-      else {
-        toast.update(loadingToastId, {
-          render: response.message || "Something went wrong",
-          type: 'error',
+          type: "error",
           isLoading: false,
           autoClose: 1900,
           containerId: "app-toast-container",
           style: { backgroundColor: "lightpink" }
-        });
-      }
+        })
+      } else {
+        toast.dismiss()
+        setLoading(true)
+        const loadingToastId = toast.loading("Loading...", { containerId: "app-toast-container" });
+        const response = await fetchData("users/register", {
+          method: "POST",
+          data: data
+        })
 
-      setLoading(false)
-      // response.isResponseOk
+        console.log(response)
+
+        if (response.isResponseOk) {
+          toast.update(loadingToastId, {
+            render: response.data?.message,
+            type: 'success',
+            isLoading: false,
+            autoClose: 1900,
+            containerId: "app-toast-container",
+            style: { backgroundColor: "lightgreen" }
+          });
+          setTimeout(() => {
+            setOtpSent(true)
+          }, 2000)
+        }
+        else {
+          toast.update(loadingToastId, {
+            render: response.message || "Something went wrong",
+            type: 'error',
+            isLoading: false,
+            autoClose: 1900,
+            containerId: "app-toast-container",
+            style: { backgroundColor: "lightpink" }
+          });
+        }
+
+        setLoading(false)
+        // response.isResponseOk
+      }
     }
   }
 
@@ -186,7 +195,7 @@ const Register = () => {
               value={step < 3 ? "Next" : "Register"}
             />
 
-            <Link className="inline-block w-full bg-[#1fbad6] text-white mt-2 rounded-lg text-center p-2 py-3" >
+            <Link to={'/driver/login'} className="inline-block w-full bg-[#1fbad6] text-white mt-2 rounded-lg text-center p-2 py-3" >
               Sign-in as Driver
             </Link>
           </form>
